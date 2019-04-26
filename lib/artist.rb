5f29022fc73c430663c997d3770c4264bd410eb1
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-26 21:56:46 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,48 @@
+class Artist
+attr_accessor :name, :songs
+@@all = []
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+def add_song(song)
+  @songs << song
+end
+
+def self.all
+  @@all
+end
+
+def save
+  @@all << self
+end
+
+def self.find_or_create_by_name(artistname)
+  # @@all.each do |artist|
+  #   if artist.name == artistname
+
+if @@all.detect {|i| i.name == artistname} == nil
+  Artist.new(artistname)
+else
+  puts "#{artistname} exists"
+end
+
+end
+
+def print_songs
+list = []
+  @songs.each do |item|
+    list << item.name
+  end
+
+
+  list = list.join("\n")
+  puts "#{list}"
+end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+def self.new_by_filename(songname)
+  songname = songname.split(" - ")
+  song = Song.new(songname[1])
+  # song.artist = Artist.find_or_create_by_name(songname[0])
+  artist = Artist.find_or_create_by_name(songname[0])
+  song
+  # @artist = artist
+  # self.artist.songs << self
+  # binding.pry
+
+end
+
+end
